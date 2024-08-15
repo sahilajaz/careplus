3ee0dfb1a089bd1a2b34a614260d6d3fbe39d12d
sahilajaz <sahilajazw@gmail.com>
2024-08-15 16:12:27 +0530

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-extrabold text-white">Home</h1>
-      <Button >Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+       <Image
+        src="/Logo.svg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="mb-12 h-10 w-fit"
+       />
+       <PatientForm/>
+       <div className="text-14-regular mt-20 flex justify-between">
+         <p className="justify-items-end text-dark-600 xl:text-left">&copy; 2024 CarePulse</p>
+         <Link href="/?admin=true" className="text-green-500">Admin</Link>
+       </div>
+      </div>
+      </section>
+      <Image
+      src="/doc.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PatientForm = () => {
+  return (
+    <div>
+      <h1>patinet form</h1>
+    </div>
+  )
+}
+
+export default PatientForm
