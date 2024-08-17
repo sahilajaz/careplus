07daff70578455b73f44fc3df0f79652eb1696e5
sahilajaz <sahilajazw@gmail.com>
2024-08-17 10:57:52 +0530

    form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,16 +6,19 @@ import { z } from "zod"
 
  
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import {Form} from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA= 'textarea',
+  PHONE_INPUT = 'phoneINput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton'
+}
+
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -41,23 +44,22 @@ export const PatientForm = () => {
 
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+         <h1 className="header">Hi there 👋</h1>
+         <p className="text-dark-700">
+            Schedule your first appointment.
+         </p>
+        </section>
+         <CustomFormField
+         fieldType={FormFieldType.INPUT}
+         control={form.control}
+         name="name"
+         label="full name"
+         placeholder="Jhon Doe"
+         iconSrc="/Lead Icon.svg"
+         iconAlt="user"
+         />
       <Button type="submit">Submit</Button>
     </form>
   </Form>
